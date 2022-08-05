08efd4c5816dd9af80485bb319f005b9de6c2375
irenekiim <79629959+irenekiim@users.noreply.github.com>
2022-08-05 14:29:43 +0900

    Initial Commit

---
+++ Catch/CatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchApp.swift
+//  Catch
+//
+//  Created by Irene on 8/5/22.
+//
+
+import SwiftUI
+
+@main
+struct CatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
